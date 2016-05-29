3d32435179c1b8d915baececfd366b22ed3d2585
svnset <r.lachmann@uni-koeln.de>
2016-05-29 15:46:48 +0200

    scale adjustments

--- galicaster/plugins/camera-ui.py
+++ galicaster/plugins/camera-ui.py
@@ -39,12 +39,13 @@ def post_init(source=None):
     new_button.show_all()
     buttonbox.add(new_button)
 
-
+#camera-ui settings window
 def open_config(button):
     global win
 
     builder = Gtk.Builder()
     builder.add_from_file(get_ui_path("camera-ui.glade"))
+
 
     win = builder.get_object("window")
 
@@ -67,26 +68,27 @@ def open_config(button):
     button = builder.get_object("rightdown")
     button.connect("clicked", move_rightdown)
 
-
     button = builder.get_object("up")
     button.connect("clicked", move_up)
-
 
     button = builder.get_object("down")
     button.connect("clicked", move_down)
 
-
     button = builder.get_object("stop")
     button.connect("clicked", stop_move)
-
 
     button = builder.get_object("home")
     button.connect("clicked", move_home)
 
-    #testing scales
-    adj = builder.get_object("adjustment1")
-    adj.connect("value-changed", set_bright)
+    button = builder.get_object("zoomin")
+    button.connect("clicked", zoom_in)
 
+    button = builder.get_object("zoomout")
+    button.connect("clicked", zoom_out)
+
+    #testing scales
+    scale = builder.get_object("scale1")
+    scale.connect("value-changed", set_bright)
 
     win.connect("delete-event", win.close)
     win.show_all()
@@ -94,53 +96,87 @@ def open_config(button):
 
 
 #testing camera functions
+
+#move
 def move_left(button):
-    pysca.pan_tilt(DEFAULT_DEVICE, pan=-7)
     print ("I move left")
+    pysca.pan_tilt(DEFAULT_DEVICE, pan=-7)
+
 
 def move_leftup(button):
+    print ("I move leftup")
     pysca.pan_tilt(DEFAULT_DEVICE, pan=-7, tilt=7)
-    print ("I move left")
+
 
 def move_leftdown(button):
+    print ("I move leftdown")
     pysca.pan_tilt(DEFAULT_DEVICE, pan=-7, tilt=-7)
-    print ("I move left")
+
 
 def move_right(button):
-    pysca.pan_tilt(DEFAULT_DEVICE, pan=7)
     print ("I move right")
+    pysca.pan_tilt(DEFAULT_DEVICE, pan=7)
+
 
 def move_rightup(button):
+    print ("I move rightup")
     pysca.pan_tilt(DEFAULT_DEVICE, pan=7, tilt=7)
-    print ("I move right")
+
 
 def move_rightdown(button):
+    print ("I move rightdown")
     pysca.pan_tilt(DEFAULT_DEVICE, pan=7, tilt=-7)
-    print ("I move right")
+
 
 def move_up(button):
-    pysca.pan_tilt(DEFAULT_DEVICE, tilt=7)
     print ("I move up")
+    pysca.pan_tilt(DEFAULT_DEVICE, tilt=7)
+
 
 def move_down(button):
-    pysca.pan_tilt(DEFAULT_DEVICE, tilt=-7)
     print ("I move down")
+    pysca.pan_tilt(DEFAULT_DEVICE, tilt=-7)
+
 
 def stop_move(button):
-    pysca.pan_tilt(DEFAULT_DEVICE, pan=0, tilt=0)
     print ("I make a break")
+    pysca.pan_tilt(DEFAULT_DEVICE, pan=0, tilt=0)
+
 
 def move_home(button):
-    pysca.pan_tilt_home(DEFAULT_DEVICE)
     print ("I move home")
+    pysca.pan_tilt_home(DEFAULT_DEVICE)
 
-def set_bright(adj):
-    old_value = adj.get_value()
-    pysca.set_exp_comp(DEFAULT_DEVICE, pysca.AUTO_EXPOSURE_BRIGHT_MODE)
+#zoom
+def zoom_in(button):
+    print ("zoom in")
+    pysca.zoom(DEFAULT_DEVICE, pysca.ZOOM_ACTION_STOP)
+    pysca.zoom(DEFAULT_DEVICE, pysca.ZOOM_ACTION_TELE, speed=3)
 
-    if adj.get_value() > old_value :
-        pysca.set_brightness(DEFAULT_DEVICE, pysca.BRIGHT_ACTION_UP)
+def zoom_out(button):
+    print ("zoom out")
+    pysca.zoom(DEFAULT_DEVICE, pysca.ZOOM_ACTION_STOP)
+    pysca.zoom(DEFAULT_DEVICE, pysca.ZOOM_ACTION_WIDE, speed=3)
+
+#scale
+def set_bright(scale):
+    old_value = 0
+    new_value = scale.get_value()
+    #stash = new_value
+    #stash2 = old_value
+
+    #print (old_value)
+    #print("here")
+    #pysca.set_exp_comp(DEFAULT_DEVICE, pysca.AUTO_EXPOSURE_BRIGHT_MODE)
+
+    if new_value > old_value :
         print ("It gets lighter")
-    elif adj.get_value < old_value :
-        pysca.set_brightness(DEFAULT_DEVICE, pysca.BRIGHT_ACTION_DOWN)
+        #old_value = new_value
+        pysca.set_brightness(DEFAULT_DEVICE, pysca.BRIGHT_ACTION_UP)
+
+    elif new_value < old_value :
         print ("It gets darker")
+        pysca.set_brightness(DEFAULT_DEVICE, pysca.BRIGHT_ACTION_DOWN)
+
+#def get_old_value(scale):
+#   old_value = float(scale.get_value())
